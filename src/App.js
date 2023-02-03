46d90700970d9e3a0cb11cf9409bfd7948bef0f7
Bartek <wierzba.b@gmail.com>
2023-02-03 19:22:04 +0100

    ComponentsSectionEnd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseItem from './components/ExpenseItem';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
 	const expenses = [
 		{
 			id: 'e1',
@@ -23,15 +24,19 @@ function App() {
 		},
 	];
 
+	// return React.createElement(
+	// 	'div',
+	// 	{},
+	// 	React.createElement('h2', {}, "Let's get started!"),
+	// 	React.createElement(Expenses, { items: expenses })
+	// );
+
 	return (
 		<div>
 			<h2> ...Let's get started!</h2>
-			<ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-			<ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[0].date}></ExpenseItem>
-			<ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[0].date}></ExpenseItem>
-			<ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[0].date}></ExpenseItem>
+			<Expenses expenses={expenses} />
 		</div>
 	);
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,21 @@
 // in React there is a convenction where we always name function this same name as component name. With btw is always started with uppercase letter and camelCase.
+import React from 'react';
+import ExpenseDate from './ExpenseDate';
+// this is how you add css file to js component in React.
+import './ExpenseItem.css';
+// Importing Card - new box instead of classic div.
+import Card from '../UI/Card';
 
-import './ExpenseItem.css'; // this is how you add css file to js component in React.
-
-function ExpenseItem(props) {
+const ExpenseItem = props => {
 	return (
-		<div className='expense-item'>
-			<div>{props.date.toISOString()}</div>
+		<Card className='expense-item'>
+			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
 				<h2>{props.title}</h2>
 			</div>
 			<div className='expense-item__price'>${props.amount}</div>
-		</div>
+		</Card>
 	);
-}
+};
 
 export default ExpenseItem;

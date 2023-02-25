58d5e21db8cad1fcd2cccf13d566d0fe457544ef
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-02-25 19:01:08 +0100

    Add New Expense and Cancel New Expense button added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,15 @@
-// in React there is a convenction where we always name function this same name as component name. With btw is always started with uppercase letter and camelCase.
 import ExpenseDate from './ExpenseDate';
-// this is how you add css file to js component in React.
-import './ExpenseItem.css';
-// Importing Card - new box instead of classic div.
 import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
+const ExpenseItem = ({ date, title, amount }) => {
 	return (
 		<li>
 			<Card className='expense-item'>
-				<ExpenseDate date={props.date} />
+				<ExpenseDate date={date} />
 				<div className='expense-item__description'>
-					<h2>{props.title}</h2>
-					<div className='expense-item__price'>${props.amount}</div>
+					<h2>{title}</h2>
+					<div className='expense-item__price'>${amount}</div>
 				</div>
 			</Card>
 		</li>
